7242a2578e4ed9e6bcf26df3731486a454191195
Art Goldhammer <art.goldhammer@gmail.com>
2023-11-01 15:09:27 -0400

    added snoopy image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,9 @@
-<h1 class="header">Eli's Math Practice</h1>
+<div class="hdr-container">
+	<div class="snoop">
+		<img src="/snoopy.png" height="40px" alt="Snoopy" />
+	</div>
+	<h1 class="header">Eli's Math Practice</h1>
+</div>
 <slot><!-- optional fallback --></slot>
 
 <style>
@@ -13,13 +18,26 @@
 		background-color: #e7e3d7;
 		/* background-image: linear-gradient(to right, rgb(189, 194, 198), white); */
 	}
+
+	.hdr-container {
+		display: flex;
+		flex-direction: row;
+		gap: 2em;
+		align-items: center;
+		justify-content: center;
+	}
+
+	/* .header::before{
+
+	} */
 	.header {
 		/* width: 80%; */
 		/* border: 1px solid black; */
+
 		padding-top: 20px;
 		padding-bottom: 20px;
-		margin: auto;
-		text-align: center;
+		/* margin: auto; */
+		/* text-align: center; */
 		/* font-size: var(--fs-2); */
 		color: var(--clr-btn);
 	}
